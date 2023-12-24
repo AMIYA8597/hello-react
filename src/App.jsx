3e5568904a97006f6cf8530cc11c06a161af2e89
AMIYA8597 <chowdhuryamiya41@gmail.com>
2023-12-24 19:23:06 +0530

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+// import Title from "./title"
+// import Product from "./Product";
+import ProductTab from "./ProductTab";
+
+// function Description() {
+
+//   return <h1>
+//     OK start Description
+//   </h1>
+//   }
+
+function App() {
+  return (
+    <ProductTab/>
+  );
+
+  // return (
+  // <div>
+  //   <h1>
+  //   Hello Amiya
+  // </h1>
+  // <Description/>
+  // <p>
+  // this is starting phase of
+  // </p>
+  //  <Title/>
+  //  </div>
+  // )
+}
+
+// export default Title
+export default App;
---
+++ src/Title.jsx
@@ -0,0 +1,16 @@
+function Title() {
+    let name = "Amiya";
+  return (
+    <>
+      <h1>React Portfolio</h1>
+      <p>
+      5 * 8 = {"calculate with own "}
+      </p>
+      <p>
+        Hi, {name.toUpperCase()}
+      </p>
+    </>
+  );
+}
+
+export default Title;
