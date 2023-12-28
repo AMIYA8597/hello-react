90c7966b1024b0b9c2531744b72acff4939cc8c7
AMIYA8597 <chowdhuryamiya41@gmail.com>
2023-12-28 09:17:06 +0530

    react handling non-click events part 3

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,19 +1,33 @@
-function helloPrint() {
-    console.log("Hello Friend!");
-    console.log("how r u!");
-};
+function handleClick() {
+  console.log("Hello Friend!");
+  console.log("how r u!");
+}
 
-function namePlease() {
-    console.log("tell me your name");
+function handleMouseOver() {
+  console.log("tell me your name");
+}
+function handleDoubleClicker() {
+  console.log("you double clicked");
 }
 
 export default function Button() {
-    return (
-      <div>
-      <button onClick={helloPrint}>CLICK HERE</button>
-      <p onClick={namePlease}>
+  return (
+    <div>
+      <button onClick={handleClick}>CLICK HERE</button>
+      {/* <p onClick={namePlease}>
         this is the demo os event in React
+      </p> */}
+
+      <p onMouseOver={handleMouseOver}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt,
+        delectus. Assumenda unde sequi nemo doloremque ullam, suscipit nulla
+        nobis inventore ratione autem et quae minus natus totam dolorum facere
+        laborum. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Quaerat, sit. Quia similique voluptate cum excepturi reprehenderit,
+        numquam sequi dolorem magni consequatur laudantium ipsa odit omnis
+        minima at quidem, voluptas quam.
       </p>
-      </div>  
-    );
+      <button onDoubleClick={handleDoubleClicker}> you double click me </button>
+    </div>
+  );
 }
